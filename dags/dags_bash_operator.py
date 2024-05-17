a65bf8638e8e2622394a6ce3ebf60aedaab8d2a6
aijinsol <jinsolkim719@gmail.com>
2024-05-17 13:08:18 +0900

    docs: update tz in dags_bash_operator.py

--- dags/dags_bash_operator.py
+++ dags/dags_bash_operator.py
@@ -5,7 +5,7 @@ from airflow.operators.bash import BashOperator
 with DAG(
     dag_id="dags_bash_operator",
     schedule="0 0 * * *",
-    start_date=pendulum.datetime(2024, 5, 13, tz="UTC"),
+    start_date=pendulum.datetime(2024, 5, 16, tz="Asia/Seoul"),
     catchup=False,
 ) as dag:
     bash_t1 = BashOperator(
