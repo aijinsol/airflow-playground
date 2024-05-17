3d58b307eba02bba3f2ad32e24772424a2527c8c
aijinsol <jinsolkim719@gmail.com>
2024-05-17 16:38:24 +0900

    feat: Add new DAG to test Python module import task

---
+++ dags/dags_python_import_func.py
@@ -0,0 +1,15 @@
+import pendulum
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from common.common_func import get_sftp
+
+with DAG(
+    dag_id="dags_python_import_func",
+    schedule="30 5 * * *",
+    start_date=pendulum.datetime(2024, 5, 17, tz="Asia/Seoul"),
+    catchup=False,
+) as dag:
+    task_get_sftp = PythonOperator(
+        task_id="task_get_sftp",
+        python_callable=get_sftp,
+    )
